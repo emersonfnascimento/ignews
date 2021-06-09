5ea18bec1a8666d0de0cb3b5c27fc2624a6c2f39
Emerson Freitas <emersonfreitasn@gmail.com>
2021-06-08 21:47:46 -0500

    definindo estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss';
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
+      <h1>
         Hello <span>NextJS</span>
       </h1>
     </>
