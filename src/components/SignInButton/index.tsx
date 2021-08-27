86e14326c1fc1ed4b8d4beed85e3c749085c113b
Emerson Freitas <emersonfreitasn@gmail.com>
2021-08-26 19:21:56 -0500

    authentication and config faunadb

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,27 +1,32 @@
-import { FaGithub } from 'react-icons/fa'
-import { FiX } from 'react-icons/fi'
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
-import styles from './styles.module.scss'
+import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  console.log(session);
+
+  return session ? (
     <button
-      type="button"
+      type='button'
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
-      <FaGithub color="#04d361" />
-      Emerson Freitas
-      <FiX color="#737380" className={styles.closeIcon} />
+      <FaGithub color='#04d361' />
+      {session.user.name}
+      <FiX color='#737380' className={styles.closeIcon} />
     </button>
   ) : (
     <button
-      type="button"
+      type='button'
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
-      <FaGithub color="#eba417" />
+      <FaGithub color='#eba417' />
       Sign in with github
-    </button >
-  )
+    </button>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
-import { GetServerSideProps } from 'next'
-import Head from 'next/head'
-import { SubscribeButton } from '../components/SubscribeButton'
-import { stripe } from '../services/stripe'
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
-import styles from './home.module.scss'
+import styles from './home.module.scss';
 
 interface HomeProps {
   product: {
     priceId: string;
     amount: number;
-  }
+  };
 }
 
 export default function Home({ product }: HomeProps) {
@@ -22,7 +22,9 @@ export default function Home({ product }: HomeProps) {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👋 Hey, welcome</span>
-          <h1>News about the <span>React</span> world</h1>
+          <h1>
+            News about the <span>React</span> world
+          </h1>
           <p>
             Get access to all the publications <br />
             <span>for {product.amount} month</span>
@@ -31,28 +33,32 @@ export default function Home({ product }: HomeProps) {
           <SubscribeButton priceId={product.priceId} />
         </section>
 
-        <img src="/images/illustration.svg" alt="Girl codding" />
+        <img src='/images/illustration.svg' alt='Girl codding' />
       </main>
     </>
-  )
+  );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const price = await stripe.prices.retrieve('price_1JKZh0BjYfYsZdjrM8GZcqiJ', {
-    expand: ['product']
-  })
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve(
+    'price_1JKZh0BjYfYsZdjrM8GZcqiJ',
+    {
+      expand: ['product'],
+    }
+  );
 
   const product = {
     priceId: price.id,
     amount: new Intl.NumberFormat('en-US', {
       style: 'currency',
-      currency: 'USD'
-    }).format(price.unit_amount / 100)
-  }
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
 
   return {
     props: {
       product,
-    }
-  }
-}
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
